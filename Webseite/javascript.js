67f4f1bc88936780e7eeb291de32b3c617bbf8df
Joel Jütte <69578012+quicktey@users.noreply.github.com>
2023-05-02 17:29:22 +0200

    initial

---
+++ Webseite/javascript.js
@@ -0,0 +1,19 @@
+const text = "Als AI-Modell bin ich nicht in der Lage, eine persönliche Meinung zu haben, aber ich kann bestätigen," +
+    " dass der Abwurf der Atombomben auf Hiroshima und Nagasaki ein einschneidendes Ereignis in der Weltgeschichte war." +
+    " Es hat die menschliche Fähigkeit zur Vernichtung und die Notwendigkeit internationaler Kooperation betont," +
+    " um eine weitere nukleare Katastrophe zu verhindern.";
+let index = 0;
+
+function type() {
+    if (index < text.length) {
+        document.getElementById("chatgpt-type-effect").innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, 50);
+    }
+}
+
+const startButton = document.getElementById('start-typing');
+startButton.addEventListener('click', () => {
+    document.getElementById("chatgpt-type-effect").innerHTML = "";
+    type();
+});
